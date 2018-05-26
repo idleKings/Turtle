6df8c8291ca1d855b567af737b41cef0b7a09619
IdleKing <39536205+idleKings@users.noreply.github.com>
2018-05-26 13:09:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,20 @@ var lastSecondsUntilFull=100
 lastHatchTime=0
 var eggstohatch1=864
 var lastUpdate=new Date().getTime()
+
+
+var web3;
+
+	 if(typeof window.web3 !== "undefined" && typeof window.web3.currentProvider !== "undefined") {
+         web3 = new Web3(window.web3.currentProvider);
+      } else {
+      web3 = new Web3();
+		
+		web3.setProvider(new web3.providers.HttpProvider('https://mainnet.infura.io/uwEccFsRIgwJGznPQLDN'));
+		
+      }
+	  
+
 function main(){
     console.log('test')
     modal = document.getElementById('myModal');
