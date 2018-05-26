ed14ab90b22c22a594d55399bd6b107c6c51aea5
IdleKing <39536205+idleKings@users.noreply.github.com>
2018-05-26 13:09:43 +0200

    Update interface.js

--- interface.js
+++ interface.js
@@ -1,4 +1,16 @@
 
+var web3;
+
+	 if(typeof window.web3 !== "undefined" && typeof window.web3.currentProvider !== "undefined") {
+         web3 = new Web3(window.web3.currentProvider);
+      } else {
+      web3 = new Web3();
+		
+		web3.setProvider(new web3.providers.HttpProvider('https://mainnet.infura.io/uwEccFsRIgwJGznPQLDN'));
+		
+      }
+	  
+
 abi=[
 	{
 		"constant": false,
